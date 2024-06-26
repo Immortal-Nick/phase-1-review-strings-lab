2ef8e2a710fe931ebd1a6bc384eb157ab5659b72
Nixon <kiplangatnixon28@gmail.com>
2024-06-26 22:42:14 +0300

    uses strings

---
+++ dist/index.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Write your code in this file!
+var currentUser = 'Manchester United';
+var welcomeMessage = "Welcome to Flatbook, + ".concat(currentUser, "!");
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+var shortGreeting = "Welcome, + ".concat(currentUser.slice(0, 1), "!");
